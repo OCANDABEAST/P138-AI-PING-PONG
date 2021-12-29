5df2889172c22294090c4e1bad3933881d6d8de4
OCANDABEAST <81494306+OCANDABEAST@users.noreply.github.com>
2021-12-28 21:58:09 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -21,10 +21,50 @@ var ball = {
     dy:3
 }
 
-function setup(){
-  var canvas =  createCanvas(700,600);
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
+
+game_status = "";
+
+function preload() {
+ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+missed = loadSound("missed.wav");
+}
+function setup() {
+  canvas = createCanvas(700,600);
+  canvas.parent('canvas');
+  
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+  
+  }
+  
+  function modelLoaded() {
+  console.log('Model Loaded!');
+  }
+  
+  function gotPoses(results)
+  {
+  if(results.lenght > 0)
+  {
+  console.log(results);
+  rightWristX = results[0].pose.rightWrist.x;
+  rightWristY = results[0].pose.rightWrist.y;
+  scoreRightWrist = results[0].pose.keypoints[10].score;
+  console.log(scoreRightWrist);
+  }
 }
 
+function startGame()
+{
+game_status = "start";
+document.getElementById("status").innerHTML = "Game Is Loading";
+}
 
 function draw(){
 
